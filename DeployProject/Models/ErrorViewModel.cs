a10e54102f59b30de8b5179d77ab7a915e447517
Andrew <vipmen5555@gmail.com>
2024-01-19 21:13:55 +0300

    Init

---
+++ DeployProject/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DeployProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
